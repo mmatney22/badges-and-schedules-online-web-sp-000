6ad4ea9aa287f44b01bc9fd9af796f7ee293f5b3
Morgan Matney <morgan.matney89@gmail.com>
2020-07-16 18:43:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,6 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
-  names.each do |name|
-    puts "Hello, my name is #{name}."
-  end
+def batch_badge_creator(attendees)
+  attendees.map {|attendee| badge_maker(attendee)}
 end
